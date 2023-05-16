a46a27200aa897829c7e015ad979bdbe08e69cae
MDEAGEWT <kmk6061602@naver.com>
2023-05-16 15:03:47 +0900

    add answer for lab3_ament_cmake

---
+++ lab/src/lab3_ament_cmake/my_cpp_pkg/src/hello.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+    printf("hello\n");
+    return 0;
+}
---
+++ lab/src/lab3_ament_cmake/my_total_pkg/src/hello_cpp.cpp
@@ -0,0 +1,9 @@
+#include "my_total_pkg/cpp_header.hpp"
+
+int main(int argc, char ** argv){
+    (void) argc;
+    (void) argv;
+
+    printf("hello world cpp\n");
+    return 0;
+}
